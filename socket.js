b165dc5e8f1a9cc8c86138b61877aa06a331ff48
Sachiko-Nakajima <43017453+Sachiko-Nakajima@users.noreply.github.com>
2020-06-18 01:45:15 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,9 +3,10 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http); 
 app.use(express.static('public'));
+var port = process.env.PORT || 3000;
 
-http.listen(3000,() => {
-    console.log('listening on *:3000');
+http.listen(port,() => {
+    console.log('listening on *:${PORT}');
 });
 
 // app.get('/', (req,res) => {
